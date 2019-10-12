42cd179055aaae30dd5834de02b53d8e446754ef
Paulo Santana <paulo.santana.r@gmail.com>
2019-10-11 21:25:30 -0300

    add websocket real-time server

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -17,6 +17,12 @@ module.exports = {
       .populate("spot")
       .execPopulate();
 
+    const ownerSocket = req.connectedUsers[booking.spot.user];
+
+    if (ownerSocket) {
+      req.io.to(ownerSocket).emit("booking_request", booking);
+    }
+
     return res.json(booking);
   }
 };
